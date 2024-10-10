4c8e1cac613fcc6682016ac5d78ed18397035fbf
PatrikRakovicky <patrik.rako13@gmail.com>
2024-10-10 09:11:55 +0200

    Revert "1."
    This reverts commit 755bfc87f6963a34389073acb872183ad4a09e5e.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/profile', [UserController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
