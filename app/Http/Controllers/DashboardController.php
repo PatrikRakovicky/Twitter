ef85a496bf3615a81d1ba112dc70106a02cae704
PatrikRakovicky <patrik.rako13@gmail.com>
2024-10-13 14:47:37 +0200

    6

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,11 +9,6 @@ class DashboardController extends Controller
 {
     public function index() {
 
-        Post::create([
-            'content' => 'test',
-            'likes' => 16, 
-        ]);
-
         $users = [
             [
                 "name" => "Fero"
